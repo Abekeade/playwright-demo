ea03b4fcf53d8f72de851fd4017a8dc5fed7df81
hannah <hannaholamipeju@gmail.com>
2024-07-25 18:46:58 +0100

    to test google page   testing of google page pennah

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -2,10 +2,10 @@ const { test, expect } = require('@playwright/test');
 
 test('fill contact form and submit', async ({ page }) => {
   // Load the initial URL
-  // await page.goto('https://sodiqyekeen.com');
+  await page.goto('https://sodiqyekeen.com');
 
   // Navigate to the contact page
-  await page.goto('https://sodiqyekeen.com/contact/');
+  await page.click('https://sodiqyekeen.com/contact/');
 
   // Fill the contact form
   await page.fill('input[name="your-name"]', 'Adekanola Hannah'); 
@@ -18,3 +18,9 @@ test('fill contact form and submit', async ({ page }) => {
   // Assert that the form was submitted successfully
   await expect(page.locator('text=Thank you for your message')).toBeVisible();
 });
+
+test('search on google.com', async({page})=>{
+    await page.goto('https://www.google.com/'); //function name - goto and the parameter is URL ('https://www.google.com/')
+    await page.fill('textarea[title="Search"]','PENNAH'); //function name is fill  and  it takes two parameters = Element to fill and value
+    await page.click('input[value="Google Search"]');
+  });
